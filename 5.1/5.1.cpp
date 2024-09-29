889fa32fe42d7bdc9af9964a82d014056bc2c47c
Anya <anyutastefurak@gmail.com>
2024-09-29 15:53:47 -0700

    first master commit

---
+++ 5.1/5.1.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double h(const double a, const double b);
+
+int main() {
+    double s, t;
+
+    cout << "s = "; cin >> s;
+    cout << "t = "; cin >> t;
+
+    double n = h(t * t, 1) * h(t * t, 1) * h(t * t, 1) + h(1, t * (s * s));
+    double d = 1 + h(s, t) * h(s, t);
+    double result = n / d;
+
+    cout << "Result = " << result << endl;
+
+    return 0;
+}
+
+double h(const double a, const double b) {
+    return (a * a) * sin(b) + (b * b) * cos(a);
+}
